7972aef37d22cc4532fd1579d0eda8b13f92ca83
supermed97 <medmedazeez@yahoo.com>
2022-09-21 15:23:58 -0400

    Initial Commit

---
+++ build/CMakeFiles/lines.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lines.dir/lines.cpp.o"
+  "lines.pdb"
+  "lines"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
